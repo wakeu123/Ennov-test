ddd908db635031003957cdbe79d48b2e9d12415d
wakeu123 <georgeswakeu21@gmail.com>
2024-08-04 02:18:04 +0100

    add doc api

--- src/main/java/com/ennov/tech/controllers/TicketController.java
+++ src/main/java/com/ennov/tech/controllers/TicketController.java
@@ -1,5 +1,7 @@
 package com.ennov.tech.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import com.ennov.tech.domains.Ticket;
 import lombok.RequiredArgsConstructor;
@@ -16,18 +18,21 @@ import static org.springframework.http.HttpStatus.*;
 @RestController
 @RequestMapping(value = "tickets")
 @RequiredArgsConstructor
+@Tag(name = "Tickets API")
 public class TicketController {
 
     private final TicketService ticketService;
 
     @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(OK)
+    @Operation(summary = "Retrieve all tickets", description = "Return a list of the tickets.")
     public List<Ticket> search() {
         log.debug("Call of retrieve all tickets");
         return this.ticketService.search();
     }
 
     @PostMapping(path = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Create an user", description = "Returns a ticket as per the id")
     public ResponseEntity<?> create(@RequestBody TicketDto ticketDto) {
         log.debug("Call of save ticket {} ", ticketDto);
         this.ticketService.create(ticketDto);
@@ -35,6 +40,7 @@ public class TicketController {
     }
 
     @ResponseStatus(OK)
+    @Operation(summary = "Retrieve a ticket by id", description = "Return a ticket as per the id")
     @GetMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Ticket getTicketById(@PathVariable("id") Long id) {
         log.debug("Call of retrieve ticket with id: {}", id);
@@ -42,6 +48,7 @@ public class TicketController {
     }
 
     @DeleteMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Delete a ticket by id", description = "Return code 202 if successfully")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         log.debug("Call of delete ticket with id: {} ", id);
         this.ticketService.delete(id);
@@ -49,6 +56,7 @@ public class TicketController {
     }
 
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Update a ticket by id", description = "Return code 204 if successfully")
     public ResponseEntity<?> update(@PathVariable("id") Long id, @RequestBody TicketDto ticketDto) {
         log.debug("Call of update ticket with id: {} with new ticket {} ", id, ticketDto);
         this.ticketService.update(id, ticketDto);
@@ -56,6 +64,7 @@ public class TicketController {
     }
 
     @PutMapping("/{ticketId}/assign/{userId}")
+    @Operation(summary = "Assign a ticket to an user", description = "Provide a ticketID and userID")
     public ResponseEntity<?> assignTicketToUser(@PathVariable("ticketId") Long ticketId, @PathVariable("userId") Long userId) {
         log.debug("Call of assign ticket id {} to user id: {}", ticketId, userId);
         this.ticketService.assignTicketToUser(ticketId, userId);
--- src/main/java/com/ennov/tech/controllers/UserController.java
+++ src/main/java/com/ennov/tech/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.ennov.tech.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import com.ennov.tech.domains.Ticket;
@@ -19,18 +21,21 @@ import static org.springframework.http.HttpStatus.*;
 @RestController
 @RequestMapping("users")
 @RequiredArgsConstructor
+@Tag(name = "Users API")
 public class UserController {
 
     private final UserService userService;
 
     @ResponseStatus(OK)
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Retrieve all users.", description = "Return a list of the users.")
     public List<AppUser> search() {
         log.debug("Call of retrieve all users");
         return this.userService.search();
     }
 
     @PostMapping()
+    @Operation(summary = "Create an user.", description = "Returns code 201 if successfully created.")
     public ResponseEntity<?> create(@RequestBody @Valid UserDto userDto) {
         log.debug("Call of save user {} ", userDto);
         this.userService.create(userDto);
@@ -39,12 +44,14 @@ public class UserController {
 
     @ResponseStatus(OK)
     @GetMapping("/{id}/ticket")
+    @Operation(summary = "Retrieve all tickets by userId.", description = "Return all tickets by userId.")
     public List<Ticket> getUserTickets(@PathVariable Long id) {
         log.debug("Call of retrieve tickets of user: {} ", id);
         return this.userService.userTickets(id);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update a user by id", description = "Return code 204 if successfully")
     public ResponseEntity<?> update(@PathVariable("id") Long id, @RequestBody UserDto userDto) {
         log.debug("Call of update user with id: {} with new ticket {} ", id, userDto);
         this.userService.update(id, userDto);
@@ -53,6 +60,7 @@ public class UserController {
 
     @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(OK)
+    @Operation(summary = "Login user.", description = "Use username and password to logging. This method return the Token.")
     public String authenticate(@RequestBody @Valid AuthenticationRequest request) {
         log.debug("Call of authenticate username: {} with password: {}", request.getUsername(), request.getPassword());
         return userService.authenticate(request);
