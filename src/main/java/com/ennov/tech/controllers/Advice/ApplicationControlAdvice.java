e7d0c52ef9cf96087e1d8efaad04492d08e3a38d
wakeu123 <georgeswakeu21@gmail.com>
2024-08-04 02:27:50 +0100

    update controller advice for add usernameException

--- src/main/java/com/ennov/tech/controllers/Advice/ApplicationControlAdvice.java
+++ src/main/java/com/ennov/tech/controllers/Advice/ApplicationControlAdvice.java
@@ -1,13 +1,14 @@
 package com.ennov.tech.controllers.Advice;
 
-import com.ennov.tech.exceptions.BadRequestException;
-import com.ennov.tech.exceptions.ConflictException;
-import com.ennov.tech.exceptions.EntityNotFoundException;
 import com.ennov.tech.exceptions.ErrorEntity;
+import com.ennov.tech.exceptions.ConflictException;
+import com.ennov.tech.exceptions.BadRequestException;
+import com.ennov.tech.exceptions.EntityNotFoundException;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -21,7 +22,13 @@ public class ApplicationControlAdvice {
     }
 
     @ResponseStatus(NOT_FOUND)
-    @ExceptionHandler(EntityNotFoundException.class)
+    @ExceptionHandler({UsernameNotFoundException.class})
+    public @ResponseBody ErrorEntity userNameExceptionHandler(UsernameNotFoundException ex) {
+        return new ErrorEntity(NOT_FOUND.value(), ex.getMessage());
+    }
+
+    @ResponseStatus(NOT_FOUND)
+    @ExceptionHandler({EntityNotFoundException.class, UsernameNotFoundException.class})
     public @ResponseBody ErrorEntity notFoundRequestHandler(EntityNotFoundException ex) {
         return new ErrorEntity(NOT_FOUND.value(), ex.getMessage());
     }
